2dc27c8ed17d0bcef062b574de3633af92fcb9cc
LeoFu9487 <leofu890806@gmail.com>
2021-04-01 09:13:07 +0200

    ssl

--- srcs/start.sh
+++ srcs/start.sh
@@ -1,7 +1,6 @@
 #ssl
 mkdir etc/nginx/ssl
-openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=emma/CN=localhost"
-#need to fix ssl
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=FR/ST=Lyon/L=Lyon/O=42 LYON/OU=yfu/CN=localhost"
 
 
 service nginx start
